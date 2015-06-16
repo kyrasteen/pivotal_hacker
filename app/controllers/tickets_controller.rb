1bfe4150a104b0b266b7aeed1a61f4ba21541a1f
Kyra Steenbock <kyra.m.steenboc@gmail.com>
2015-06-16 11:45:35 -0600

    base functionality

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -9,12 +9,21 @@ class TicketsController < ApplicationController
     description = params[:ticket][:description]
     status = params[:ticket][:status]
     ticket = current_board.tickets.new(title: title,description: description, status: status.to_i)
+    # ticket = Ticket.new(ticket_params)
     if ticket.save
       redirect_to board_path(current_board)
     else
       flash[:error] = "something went wrong"
       redirect_to board_path(current_board)
     end
+  end
+
+  def update
+    ticket = Ticket.find_by(id: params[:id])
+    status = params[:status]
+    status_number = Ticket.statuses[status].to_i + 1
+    ticket.update_attributes(status: status_number)
+    redirect_to board_path(current_board)
   end
 
   def current_board
@@ -26,4 +35,5 @@ class TicketsController < ApplicationController
   def ticket_params
     params.require(:ticket).permit(:title, :description, :status)
   end
+
 end
--- spec/features/tickets_spec.rb
+++ spec/features/tickets_spec.rb
@@ -12,4 +12,24 @@ describe "Board tickets" do
     expect(page).to have_content("styles")
     expect(board.tickets.count).to eq(1)
   end
+
+  it "can change status of ticket from backlog to sprint" do
+    board = Board.create(title: "my project")
+    ticket = board.tickets.create(title: "first ticket", description:"", status:0)
+    visit board_path(board)
+    click_link_or_button("Move to Sprint")
+    within("#current-sprint") do
+      expect(page).to have_content(ticket.title)
+    end
+  end
+
+  it "can change status of ticket from sprint to in-progress" do
+    board = Board.create(title: "my project")
+    ticket = board.tickets.create(title: "first ticket", description:"", status:1)
+    visit board_path(board)
+    click_link_or_button("Move to In Progress")
+    within("#in-progress") do
+      expect(page).to have_content(ticket.title)
+    end
+  end
 end
